0764ff70c6548c9d6b25ec767daaa8fa6094c51d
tanay001 <damtanay@gmail.com>
2020-08-03 23:21:08 +0530

    Initial commit

---
+++ Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default ({ name }) => <h3> 
+My first websites 
+   </h3>;
+
+    
+ 
+
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-6xb7ja
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-6xb7ja)
